d79afdc5e544b0d79493e6d5f38ab404ed4d90dc
Loganathan <109952498+Loganathanlogu@users.noreply.github.com>
2022-07-27 21:52:55 +0530

    Add files via upload

---
+++ variable/program/variable.java
@@ -0,0 +1,15 @@
+package com.variable.program;
+
+public class variable {
+	public static void main(String[] args) {
+		int a=10;
+		float b=55.6f;
+		String name="logu";
+		char lastname='S';
+		boolean loganathan=false;
+		System.out.println(a+b+"  "+"name is:"+name+lastname+" "+loganathan);
+		
+		
+		
+	}
+}
---
+++ variables/example/variablesexample.java
@@ -0,0 +1,16 @@
+package com.variables.example;
+
+public class variablesexample {
+	int b=99;
+	public static void main(String[] args) {
+	int a=5;
+	System.out.println(a);
+	variablesexample va = new variablesexample();
+	va.display();
+	}
+
+	 void display() {
+		 int s=6;
+		 System.out.println(b);
+	 }
+}
